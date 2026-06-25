4f5c20e0183f48847fcef4753a0440ba69b291bf
beatrizlima-tech <bealima.tech@gmail.com>
2026-06-25 14:38:08 -0300

    feat: implementa CRUD completo de produtos

--- src/main/java/br/com/cotiinformatica/produtos_api/controllers/ProdutoController.java
+++ src/main/java/br/com/cotiinformatica/produtos_api/controllers/ProdutoController.java
@@ -4,17 +4,16 @@ import br.com.cotiinformatica.produtos_api.dtos.ProdutoRequestDto;
 import br.com.cotiinformatica.produtos_api.dtos.ProdutoResponseDto;
 import br.com.cotiinformatica.produtos_api.entities.Produto;
 import br.com.cotiinformatica.produtos_api.repositories.ProdutoRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/produtos")
 public class ProdutoController {
 
     @PostMapping("criar")
-    public String criar(@RequestBody ProdutoRequestDto dto){
-        try{
+    public ResponseEntity<String> criar(@RequestBody ProdutoRequestDto dto) {
+        try {
 
             //Capturar os dados do produto
             var produto = new Produto();
@@ -24,50 +23,112 @@ public class ProdutoController {
             produto.setPreco(dto.preco());
             produto.setQuantidade(dto.quantidade());
 
-            // Salvando o produto no banco de dados
+            //Salvando o produto no banco de dados
             var repository = new ProdutoRepository();
             repository.inserir(produto);
 
-            return "Produto cadastrado com sucesso";
+            return ResponseEntity.status(201).body("Produto cadastrado com sucesso.");
         }
-        catch(Exception e){
-            return "Erro: "+e.getMessage();
+        catch(Exception e) {
+            return ResponseEntity.status(500).body("Erro: " + e.getMessage());
         }
     }
 
-    @PutMapping("alterar")
-    public String alterar(){
-        return "Produto alterado com sucesso"; //TODO
+    @PutMapping("alterar/{id}")
+    public ResponseEntity<String> alterar(@PathVariable Integer id, @RequestBody ProdutoRequestDto dto) {
+        try {
+
+            //Caturar os dados do produto
+            var produto = new Produto();
+            produto.setId(id);
+            produto.setNome(dto.nome());
+            produto.setDescricao(dto.descricao());
+            produto.setPreco(dto.preco());
+            produto.setQuantidade(dto.quantidade());
+
+            var repository = new ProdutoRepository();
+
+            //Atualizar o produto no banco de dados e verificar a resposta
+            if(repository.atualizar(produto)) {
+                return ResponseEntity.status(200).body("Produto atualizado com sucesso.");
+            }
+            else {
+                return ResponseEntity.status(404).body("Produto não encontrado para edição.");
+            }
+        }
+        catch(Exception e) {
+            return ResponseEntity.status(500).body("Erro: " + e.getMessage());
+        }
     }
 
-    @DeleteMapping("excluir")
-    public String excluir(){
-        return "Produto excluado com sucesso"; //TODO
+    @DeleteMapping("excluir/{id}")
+    public ResponseEntity<String> excluir(@PathVariable Integer id) {
+        try {
+            var repository = new ProdutoRepository();
+
+            //Excluir o produto no banco de dados e verificar a resposta
+            if(repository.excluir(id)) {
+                return ResponseEntity.status(200).body("Produto excluído com sucesso.");
+            }
+            else {
+                return ResponseEntity.status(404).body("Produto não encontrado para exclusão.");
+            }
+        }
+        catch(Exception e) {
+            return ResponseEntity.status(500).body("Erro: " + e.getMessage());
+        }
     }
 
     @GetMapping("listar")
-    public List<ProdutoResponseDto> listar(String nome){
-
+    public ResponseEntity<?> listar(String nome) {
         try {
             //Instanciando o repositório
             var repository = new ProdutoRepository();
-            //Executando e retornando a lista de produtos no banco
+            //Executando e capturando a lista de produtos no banco
             var lista = repository.obterPorNome(nome);
 
             //Copiando os registros obtidos do banco para uma lista de ProdutoResponseDto
-            return lista.stream()
+            var response = lista.stream()
                     .map(item -> new ProdutoResponseDto(
                             item.getId(),
                             item.getNome(),
                             item.getDescricao(),
                             item.getPreco(),
                             item.getQuantidade(),
-                            item.getPreco()* item.getQuantidade()
-                            )).toList();
+                            item.getPreco() * item.getQuantidade()
+                    )).toList();
+
+            return ResponseEntity.status(200).body(response);
         }
-        catch (Exception e){
-            e.printStackTrace(); //Imprimindo o log do erro no terminal do servidos
-            return null;
+        catch(Exception e) {
+            return ResponseEntity.status(500).body("Erro: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("obter/{id}")
+    public ResponseEntity<?> obter(@PathVariable Integer id) {
+        try {
+            //Instanciando o repositório
+            var repository = new ProdutoRepository();
+            //Executando e capturando um produto através do ID
+            var produto = repository.obterPorId(id);
+
+            if(produto == null) {
+                return ResponseEntity.status(404).body("Produto não encontrado.");
+            }
+
+            var response = new ProdutoResponseDto(
+                    produto.getId(),
+                    produto.getNome(),
+                    produto.getDescricao(),
+                    produto.getPreco(),
+                    produto.getQuantidade(),
+                    produto.getPreco() * produto.getQuantidade()
+            );
+            return ResponseEntity.status(200).body(response);
+        }
+        catch(Exception e) {
+            return ResponseEntity.status(500).body("Erro: " + e.getMessage());
         }
     }
 }
--- src/main/java/br/com/cotiinformatica/produtos_api/repositories/ProdutoRepository.java
+++ src/main/java/br/com/cotiinformatica/produtos_api/repositories/ProdutoRepository.java
@@ -7,20 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProdutoRepository {
-
-    // Método para inserir um produto na tabela no banco de dados
-
-    public void inserir(Produto produto) throws Exception{
+    /*
+        Método para inserir um produto na tabela do banco de dados
+     */
+    public void inserir(Produto produto) throws Exception {
 
         //Abrir conexão com o banco de dados
-
         var factory = new ConnectionFactory();
         var connection = factory.getConnection();
 
         //Executar o comando SQL para inserir um registro na tabela de produtos
         var statement = connection.prepareStatement("""
             insert into produtos(nome, descricao, preco, quantidade)
-            values (?,?,?,?)
+            values(?,?,?,?)
         """);
 
         statement.setString(1, produto.getNome());
@@ -34,11 +33,58 @@ public class ProdutoRepository {
     }
 
     /*
-        Método para retonar uma lista com produtos ativos do banco de dados
+        Método para atualizar os dados do produto
+     */
+    public boolean atualizar(Produto produto) throws Exception {
+
+        var factory = new ConnectionFactory();
+        var connection = factory.getConnection();
+
+        var statement = connection.prepareStatement("""
+            update produtos 
+            set nome = ?, descricao = ?, preco = ?, quantidade = ?, data_atualizacao = now()
+            where id = ?
+        """);
+
+        statement.setString(1, produto.getNome());
+        statement.setString(2, produto.getDescricao());
+        statement.setDouble(3, produto.getPreco());
+        statement.setInt(4, produto.getQuantidade());
+        statement.setInt(5, produto.getId());
+        var result = statement.executeUpdate();
+
+        connection.close();
+
+        return result > 0; //true ou false
+    }
+
+    /*
+        Método para excluir um produto (exclusão lógica)
+     */
+    public boolean excluir(Integer id) throws Exception {
+
+        var factory = new ConnectionFactory();
+        var connection = factory.getConnection();
+
+        var statement = connection.prepareStatement("""
+            update produtos 
+            set ativo = 0, data_exclusao = now()
+            where ativo = 1 and id = ?
+        """);
+
+        statement.setInt(1, id);
+        var result = statement.executeUpdate();
+
+        connection.close();
+
+        return result > 0; //true ou false
+    }
+
+    /*
+        Método para retornar uma lista com produtos ativos do banco de dados
         e filtrando pelo nome do produto.
      */
-
-    public List<Produto> obterPorNome(String nome) throws Exception{
+    public List<Produto> obterPorNome(String nome) throws Exception {
 
         var factory = new ConnectionFactory();
         var connection = factory.getConnection();
@@ -46,7 +92,7 @@ public class ProdutoRepository {
         var statement = connection.prepareStatement("""
             select id, nome, descricao, preco, quantidade
             from produtos
-            where nome ilike ?
+            where ativo = 1 and nome ilike ?
             order by nome
         """);
 
@@ -55,7 +101,7 @@ public class ProdutoRepository {
 
         var lista = new ArrayList<Produto>(); //Lista de produtos vazia
 
-        while(result.next()){ //Lendo cada produto obtido do banco de dados
+        while(result.next()) { //Lendo cada produto obtido do banco de dados
 
             var produto = new Produto(); //Criando uma instância de produto
 
@@ -66,10 +112,45 @@ public class ProdutoRepository {
             produto.setQuantidade(result.getInt("quantidade"));
 
             lista.add(produto); //Adicionando o produto na lista
-
         }
+
         connection.close();
 
         return lista;
+    }
+
+    /*
+        Método para consultar 1 produto através do ID
+     */
+    public Produto obterPorId(Integer id) throws Exception {
+
+        var factory = new ConnectionFactory();
+        var connection = factory.getConnection();
+
+        var statement = connection.prepareStatement("""
+            select id, nome, descricao, preco, quantidade
+            from produtos
+            where ativo = 1 and id = ?
+        """);
+
+        statement.setInt(1, id);
+        var result = statement.executeQuery();
+
+
+        Produto produto = null;
+
+        if(result.next()) {
+            produto = new Produto();
+
+            produto.setId(result.getInt("id"));
+            produto.setNome(result.getString("nome"));
+            produto.setDescricao(result.getString("descricao"));
+            produto.setPreco(result.getDouble("preco"));
+            produto.setQuantidade(result.getInt("quantidade"));
+        }
+
+        connection.close();
+
+        return produto;
     }
 }
